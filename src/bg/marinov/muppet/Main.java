bdbad6adca3e42465d2c6ed8423e411db9cdb8cf
gcnew <gcnew@abv.bg>
2015-06-23 21:45:35 +0300

    - implemented cli

--- src/bg/marinov/muppet/Main.java
+++ src/bg/marinov/muppet/Main.java
@@ -1,13 +1,44 @@
 package bg.marinov.muppet;
 
-public class Main {
-	public static void main(String[] args) {
-		try {
-			final String s = TemplateContainer.eval("<? echo('Hello')");
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
 
-			System.out.println(s);
-		} catch (Exception e) {
-			e.printStackTrace();
+public class Main {
+	public static void main(final String[] args) {
+		final boolean showHelp = Arrays.asList(args).stream().anyMatch(x -> "--help".equals(x));
+
+		if ((args.length != 1) || showHelp) {
+			System.err.println("Syntax: muppet <file>");
+			System.exit(-1);
 		}
+
+		final String fileName = args[0];
+		final File file = new File(fileName);
+
+		if (!file.exists()) {
+			System.err.println("File not found: " + fileName);
+			System.exit(-1);
+		}
+
+		try (final FileInputStream fs = new FileInputStream(file)) {
+			final String contents = readStream(fs);
+			final String result = TemplateContainer.eval(contents);
+
+			System.out.print(result);
+		} catch (Exception e) {
+			System.err.println("Building template failed!");
+			e.printStackTrace(System.err);
+
+			System.exit(-1);
+		}
+	}
+
+	@SuppressWarnings("resource")
+	private static String readStream(final InputStream aStream) {
+		final Scanner s = new Scanner(aStream).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
 	}
 }
